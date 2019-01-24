c4971663d8e45daaf686337aa64946427b5519df
Steven Sommer <steven.sommer@mail.weber.edu>
2019-01-24 08:43:43 -0700

    new skeleton

---
+++ mod3/cartesian.c
@@ -0,0 +1,44 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  cartesian.c
+ *
+ *    Description:  calculate the cartesian coordinates based on:
+ *                  radius and theta angle(degrees)
+ *
+ *        Version:  1.0
+ *        Created:  01/24/2019 08:36:15 AM
+ *       Revision:  none
+ *       Compiler:  gcc cartesian.c -o cartesian.out -lm
+ *
+ *         Author:  Steven Sommer (), steven.sommer@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    int radius;
+    int theteAngle;
+    //1) Capture user inut for radius and theta angle (degrees)
+    printf("Enter your radius: ");
+    scanf("%d", radius);
+
+    //2) convert the theta to radians
+
+    //3) calculate your cartesian coordinates
+
+    //4) Print the result
+
+    return 0;
+}
+// Function Definitions
+
+
