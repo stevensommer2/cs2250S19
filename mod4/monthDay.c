d6060b1c261b29f22d7fd0f8e0b465443efe9bde
Steven Sommer <steven.sommer@mail.weber.edu>
2019-01-31 09:21:09 -0700

    initial

---
+++ mod4/monthDay.c
@@ -0,0 +1,38 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  monthDay.c
+ *
+ *    Description:  Take month and day from user, then verify 
+ *                  entries are valid
+ *
+ *        Version:  1.0
+ *        Created:  01/31/2019 09:16:59 AM
+ *       Revision:  none
+ *       Compiler:  gcc monthDay.c -o monthDay.out
+ *
+ *         Author:  Steven Sommer (), steven.sommer@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    // 1) capture user input
+
+    // 2) Use IF to validate month
+
+    // 3) user Switch to validate day of the month
+
+    return 0;
+}
+// Function Definitions
+
+
