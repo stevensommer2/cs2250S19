33315ed8496183327ddf2cf6c66b5b911e0b65f5
Steven Sommer <steven.sommer@mail.weber.edu>
2019-02-05 10:08:50 -0700

    works to task 6 almost, needs to get class average

--- mod4/while.c
+++ mod4/while.c
@@ -16,12 +16,14 @@
  * =====================================================================================
  */
 #include <stdio.h>
-
+#include <stdbool.h> 
 // Constants
 #define MAXCOUNT 5
 #define MINCOUNT 2
 #define MINGRADE 0
 #define MAXGRADE 100
+#define MAXSTUDENTS 3
+
 // Function Prototypes
 
 // Main Function
@@ -35,48 +37,52 @@ int main()
     //         set it to MAXCOUNT
     // Task 5: Support multiple student entries. Calculate class average.
     // Task 6: Display student letter grade.
-    int count = 0;
+    int gradeCount = 0, studentCount = 1;
     float grade, avg;
     float total = 0;
-    int studentCount = 0;
+    int hwCount;
 
-    printf("How many grades would you like to average? between  %d and %d?\n", MINCOUNT,  MAXCOUNT);
-    scanf("%d", &studentCount);
 
-        if(studentCount > MAXCOUNT)  {
-            printf("%d is more than %d. Setting your entry to %d\n",
-                    studentCount, MAXCOUNT, MAXCOUNT);
-            studentCount = MAXCOUNT;
-        }
-        if(studentCount < MINCOUNT)  {
-            printf("%d is less than %d. Setting your entry to %d\n",
-                    studentCount, MINCOUNT, MINCOUNT);
-            studentCount = MINCOUNT;
-        }
-
-    while (count < MAXCOUNT) 
+    while(studentCount <= MAXSTUDENTS)             //Loop over students
     {
-        printf("Enter %d hw grades(%d-%d): \n ", count + 1, MINGRADE, MAXGRADE);
-        scanf("%f", &grade);
-
+        printf("Processing %d student grades\n", studentCount);
+        printf("How many grades would you like to average? between  %d and %d?\n", MINCOUNT,  MAXCOUNT);
+        scanf("%d", &hwCount);
+        if(hwCount > MAXCOUNT)  {
+            printf("%d is more than %d. Setting your entry to %d\n",
+                    hwCount, MAXCOUNT, MAXCOUNT);
+            hwCount = MAXCOUNT;
+        }
+        if(hwCount < MINCOUNT)  {
+            printf("%d is less than %d. Setting your entry to %d\n",
+                    hwCount, MINCOUNT, MINCOUNT);
+            hwCount = MINCOUNT;
+        }
+        while (gradeCount < MAXCOUNT) 
+        {
+            printf("Enter %d hw grades(%d-%d): \n ", gradeCount + 1, MINGRADE, MAXGRADE);
+            scanf("%f", &grade);
             if (grade > MAXGRADE || grade < MINGRADE)
             {
                 printf("invalid input. Please try again.\n");
-
-            continue;   //invalid input
+                continue;   //invalid input
             }
-        total += grade; //add up grades
+            total += grade; //add up grades
+            //update test condition: sentinel
+            gradeCount++;
+            if(gradeCount == hwCount)
+            {
+                break;      //reach the hwCount
+            }
+        }               //End of one student 
+        avg = total / hwCount;   //calculate the average
+        printf("Your avg is [%6.2f]\n", avg);
+        //reset value
+        gradeCount = 0;
+        total = 0;
 
-        
-        //update test condition: sentinel
-        count++;
-        if(count == studentCount)
-        {
-            break;      //reach the studentCount
-        }
-    }
-    avg = total / studentCount;   //calculate the average
-    printf("Your avg is [%6.2f]\n", avg);
+        studentCount ++;
+    }           //End of students loop
     printf("Tchuss Schwein\n");
 
 
