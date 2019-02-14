70d17224c447ba21f0b868f85a72f2a437fa036f
Steven Sommer <steven.sommer@mail.weber.edu>
2019-02-14 09:29:45 -0700

    does not work

---
+++ mod6/mainArgs.c
@@ -0,0 +1,33 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  mainArgs.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/14/2019 09:17:39 AM
+ *       Revision:  none
+ *       Compiler:  gcc mainArgs.c -o mainArgs.out
+ *
+ *         Author:  Steven Sommer (), steven.sommer@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main(int argc, char* argc[])
+{
+    printf ( "Argument count is : %d\n", argc);
+
+    return 0;
+}
+// Function Definitions
+
+
