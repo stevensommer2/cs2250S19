2a5f35162a1e12422d1571d616a522542150d1f2
Steven Sommer <steven.sommer@mail.weber.edu>
2019-03-12 09:25:51 -0600

    initial

---
+++ mod8/calculator.h
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  calculator.h
+ *
+ *    Description:  Calculator library
+ *
+ *        Version:  1.0
+ *        Created:  02/28/2019 09:56:53 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Steven Sommer (), steven.sommer@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#ifndef  CALCULATOR__INC__
+#define  CALCULATOR__INC__
+//constants
+
+//prototypes
+int Add_Numbers(int num1, int num2);
+int Sub_Numbers(int num1, int num2);
+
+#endif /* ----- #ifndef CALCULATOR__INC__ ----- */
+
