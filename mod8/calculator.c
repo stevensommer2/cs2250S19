6950684361eb7273fc5e8126013539fc05482833
Steven Sommer <steven.sommer@mail.weber.edu>
2019-03-12 09:25:13 -0600

    initial

---
+++ mod8/calculator.c
@@ -0,0 +1,32 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  calculator.c
+ *
+ *    Description:  Calculator library function implementation file
+ *
+ *        Version:  1.0
+ *        Created:  02/28/2019 09:56:53 AM
+ *       Revision:  none
+ *       Compiler:  gcc calculator.c -o calculator.out -lm
+            Usage:  #include "calculator.h"
+ *
+ *         Author:  Steven Sommer (), steven.sommer@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "calculator.h"
+
+// Function Definitions
+ int Add_Numbers(int num1, int num2)
+{
+    return num1 + num2;
+}
+int Sub_Numbers(int num1, int num2)
+{
+    return num1 - num2;
+}
+
