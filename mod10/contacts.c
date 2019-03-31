b5782091e42397dbde2f883409c80890feea3112
stevensommer2 <46788601+stevensommer2@users.noreply.github.com>
2019-03-31 15:19:08 -0600

    Update contacts.c
    added code

--- mod10/contacts.c
+++ mod10/contacts.c
@@ -1,3 +1,5 @@
+
+
 /*
  * =====================================================================================
  *
@@ -6,12 +8,12 @@
  *    Description:  
  *
  *        Version:  1.0
- *        Created:  03/27/2019 09:02:12 PM
+ *        Created:  03/31/2019 03/18/19 PM
  *       Revision:  none
- *       Compiler:  gcc contacts.c -o contacts.out -lm
-            Usage:  ./contacts.out
+ *       Compiler:  gcc contacts.c -o contacts.out
+ *          Usage:  ./contacts.out
  *
- *         Author:  Steven Sommer (), steven.sommer@mail.weber.edu
+ *         Author:  Steven Sommer(), Stevensommer@mail.weber.edu
  *   Organization:  WSU
  *
  * =====================================================================================
@@ -19,13 +21,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "contacts.h"
-
 // Constants
 
-// Function Prototypes
-
-// Main Function
-
 // Function Definitions
+
+ContactNode CreateContactNode(struct ContactNode* contact, char name[50], char number[50], ContactNode* address)
+{
+    
+strcpy(contact->contactName, name);
+strcpy(contact->contactPhoneNum, number);
+contact->nextNodePtr=address;
+
+
+
+
+
+    
+        
+        return *contact;
+}
+void InsertContactAfter(ContactNode* contact, ContactNode* nextcontact)
+{
+    ContactNode* tmpNext = NULL;
+
+    tmpNext = contact->nextNodePtr;
+    contact->nextNodePtr = nextcontact;
+    nextcontact->nextNodePtr = tmpNext;
+    return;
+}
+ContactNode* GetNextContact(ContactNode* contact)
+{
+     return contact->nextNodePtr;
+}
+void PrintContactNode(const ContactNode* contact)
+{
+
+    printf("Name: %s\n", contact->contactName); //may need to fix how this handles the new lines
+    printf("Phone number: %s\n\n", contact->contactPhoneNum);//same as above
+
+    return;
+}
 
 
