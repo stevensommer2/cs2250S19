f3687be0387534948f5ece09fae43470d5d07d66
Steven Sommer <steven.sommer@mail.weber.edu>
2019-04-05 22:03:32 -0600

    initiali commit

---
+++ mod9/homework.c
@@ -0,0 +1,40 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  homework.c
+ *
+ *    Description:  doing the homework
+ *
+ *        Version:  1.0
+ *        Created:  04/05/2019 10:02:57 PM
+ *       Revision:  none
+ *    Compiler(C):  gcc homework.c -o homework.out -lm
+ *  Compiler(C++):  g++ homework.c -o homework.out -lm
+            Usage:  ./homework.out
+ *
+ *         Author:  Steven Sommer (), steven.sommer@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+//For C code
+#include <stdio.h>
+#include <stdlib.h>
+
+//For C++ code
+//#include <iostream>
+//using namespace std;
+
+// Constants and globals
+
+// Function Prototypes
+
+// Main Function
+int main(int argc, char* argv[])
+{
+
+    return 0;
+}
+// Function Definitions
+
+
