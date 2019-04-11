5a7a8c922e8b71e8f442461dc5f8b325437f42d8
stevensommer2 <46788601+stevensommer2@users.noreply.github.com>
2019-04-11 08:46:17 -0600

    hugos

--- mod13/movie_list.cpp
+++ mod13/movie_list.cpp
@@ -17,69 +17,93 @@
  *
  * =====================================================================================
  */
-//For C code
-#include <stdio.h>
-#include <iomanip>  //pretty output
-#include <vector>   //for vectors
-#include <string>   //for strings
-#include <fstream>  //for file stream
-#include <sstream>  //to read strings with spaces
-#include <stdlib.h>
-#include "movie.h"
+
+// For C++ Code
+#include <iostream>
+#include <iomanip>      // pretty output
+#include <vector>       // for vectors
+#include <string>       // for strings
+#include <fstream>      // for file stream
+#include <sstream>      // to read strings with spaces
+#include "Movie.h"
 using namespace std;
-
-//For C++ code
-//#include <iostream>
-//using namespace std;
-
-// Constants and globals
-
+// Constants and Globals
+const string movie_file = "movies.txt";
 // Function Prototypes
 void display_menu();
-vector<Movie> read_Movies_from_file();
-
+vector<Movie> read_movies_from_file();
+void view_movies(const vector<Movie>& movies);
 // Main Function
 int main(int argc, char* argv[])
 {
-
-    cout << "The Movie List program" << endl << endl;
+    cout << "The Movie List program\n" << endl;
     display_menu();
     vector<Movie> movies = read_movies_from_file();
+    char command = 'v';
+    while(command != 'x')
+    {
+        view_movies(movies);
 
+        command = 'x';
+    } // end of while command
 
     return 0;
 }
-// Function Definitions
+// Function Defenitions
+void view_movies(const vector<Movie>& movies)
+{
+    int width = 8;
+    cout << left
+        << setw(width / 2) << " "
+        << setw(width * 4) << "TITLE"
+        << setw(width) << "YEAR"
+        << setw(width) << "STARS" << endl;
+    // Loop over vector and get info using your getters
+    int number = 1;
+    for(Movie movie : movies)
+    {
+        cout << setw(width / 2) << number
+            << setw(width*4) << movie.get_title()
+            << setw(width) << movie.get_year()
+            << setw(width) << movie.get_stars() << endl;
+        number++;
+    }
+    cout << endl;
+}
 
-vector<Movie> read_Movies_from_file()
+//
+vector<Movie> read_movies_from_file()
 {
     vector<Movie> movies;
     // Read file
     ifstream input_file(movie_file);
     if(input_file) // if file opened successfully
     {
-        string line;
+        string line; 
         while(getline(input_file, line))
         {
             stringstream ss(line);  // read the line and cast it as string
-            //Now ready to parse the line
+            // Now ready to parse the line
             string title;
             int year, stars;
-            getline(ss, title, '\t');  //read title until you get a \t (tab)
-            ss >> year >> stars;   //get year and stars
-            cout >> title << " " << year << " " << stars << endl
-        }
+            getline(ss, title, ','); // read title until you get a \t
+            ss >> year >> stars;        // get year and stars
+//            cout << title << " year=" << year << " stars=" << stars << " extra "<< endl; // TODO Debug 
+            // Create and add movie object to vector
+            movies.push_back(Movie(title, year, stars));
+        } // end of loop over file
+        input_file.close(); // close file
+    }// end of if statement
 
-    }
     return movies;
 }
-
 void display_menu()
 {
     cout << "COMMANDS" << endl
-         << "v - View movie list" << endl
-         << "a - Add a movie" << endl
-         << "d - Delete a movie" << endl
-         << "x - Exit" << endl << endl;
+        << "v - View movie list" << endl
+        << "a - Add a movie" << endl
+        << "d - Delete a movie" << endl
+        << "x - Exit" << endl << endl;
 }
+
 
