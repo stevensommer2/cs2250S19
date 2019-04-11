ade4d806b7166da6b5b860021b1a28c1723bc2af
stevensommer2 <46788601+stevensommer2@users.noreply.github.com>
2019-04-11 09:29:38 -0600

    update

--- mod13/movie_list.cpp
+++ mod13/movie_list.cpp
@@ -18,6 +18,25 @@
  * =====================================================================================
  */
 
+/*
+ * =====================================================================================
+ *
+ *       Filename:  movie_list.cpp
+ *
+ *    Description:  Create a movie list program. Loads information from txt file
+ *                  then, it presents the user with a menu to interact with the data.
+ *
+ *        Version:  1.0
+ *        Created:  04/09/2019 09:41:40 AM
+ *       Revision:  none
+ *       Compiler (C++):  g++ movie_list.cpp Movie.cpp -o movie_list.out
+ *          Usage:  ./movie_list.out 
+ *
+ *         Author:  Hugo Valle (), hugovalle1@weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
 // For C++ Code
 #include <iostream>
 #include <iomanip>      // pretty output
@@ -25,6 +44,7 @@
 #include <string>       // for strings
 #include <fstream>      // for file stream
 #include <sstream>      // to read strings with spaces
+#include <stdio.h>
 #include "Movie.h"
 using namespace std;
 // Constants and Globals
@@ -33,18 +53,37 @@ const string movie_file = "movies.txt";
 void display_menu();
 vector<Movie> read_movies_from_file();
 void view_movies(const vector<Movie>& movies);
+void add_movie(vector<Movie>& movies);
+Movie get_movie();
+
 // Main Function
 int main(int argc, char* argv[])
 {
     cout << "The Movie List program\n" << endl;
-    display_menu();
     vector<Movie> movies = read_movies_from_file();
     char command = 'v';
     while(command != 'x')
     {
-        view_movies(movies);
-
-        command = 'x';
+        display_menu();
+        cout << "Command: ";
+        cin >> command;
+        switch(command)
+        {
+            case 'v':
+                view_movies(movies);
+                break;
+            case 'a':
+                 add_movie(movies);
+                break;
+            case 'd':
+                // delete_movie(movies);
+                break;
+            case 'x':
+                cout << "Bye!" << endl << endl;
+                break;
+            default:
+                cout << "Not a valid command. Please try again." << endl;
+        }  // end of switch(command)
     } // end of while command
 
     return 0;
@@ -77,17 +116,24 @@ vector<Movie> read_movies_from_file()
     vector<Movie> movies;
     // Read file
     ifstream input_file(movie_file);
+    // Read the Data from the file  as Strings
     if(input_file) // if file opened successfully
     {
         string line; 
         while(getline(input_file, line))
         {
-            stringstream ss(line);  // read the line and cast it as string
+            // Use for breaking words
+            stringstream ss(line);  
             // Now ready to parse the line
-            string title;
+            string title, temp;
             int year, stars;
-            getline(ss, title, ','); // read title until you get a \t
-            ss >> year >> stars;        // get year and stars
+            getline(ss, title, ','); // read title until you get a ','
+            
+            getline(ss, temp, ','); // read title until you get a ','
+            year = stoi(temp); // convert string to integer
+            
+            getline(ss, temp, ','); // read title until you get a ','
+            stars = stoi(temp);
 //            cout << title << " year=" << year << " stars=" << stars << " extra "<< endl; // TODO Debug 
             // Create and add movie object to vector
             movies.push_back(Movie(title, year, stars));
@@ -107,3 +153,49 @@ void display_menu()
 }
 
 
+Movie get_movie()
+{
+    string title;
+    cout << "Title: ";
+    cin.ignore(1000, '\n');
+    getline(cin, title);
+
+    int year;
+    cout << "Year: ";
+    cin >> year;
+
+    int stars;
+    cout << "Stars (1-5): ";
+    cin >> stars;
+
+    Movie movie(title, year, stars);
+    return movie;
+}
+
+void add_movie(vector<Movie>& movies) 
+{
+    Movie movie = get_movie();
+    // check if movie already exists
+    bool already_exists = false;
+    for (Movie& m : movies) 
+    {
+        // Check if movie record already exists
+        if (m.iequals(movie)) 
+        {
+            already_exists = true;
+            // Updating the star number
+            m.set_stars(movie.get_stars());
+            break;
+        }
+    }
+
+    if (already_exists)
+    {
+        cout << movie.get_title() << " was updated.\n\n";
+    }
+    else 
+    {
+        movies.push_back(movie);  // add it to list
+        cout << movie.get_title() << " was added.\n\n";
+    }
+}
