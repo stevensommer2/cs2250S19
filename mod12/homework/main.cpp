7fc46950f16e783406c0120ba7a5fa3c4b435889
stevensommer2 <46788601+stevensommer2@users.noreply.github.com>
2019-04-14 19:53:40 -0600

    hugos template

--- mod12/homework/main.cpp
+++ mod12/homework/main.cpp
@@ -17,24 +17,56 @@
  *
  * =====================================================================================
  */
-//For C code
-#include <stdio.h>
-#include <stdlib.h>
-
-//For C++ code
-//#include <iostream>
-//using namespace std;
-
-// Constants and globals
-
-// Function Prototypes
+// For C++ Code
+#include <iostream>
+#include <iomanip>
+#include "ItemToPurchase.h"
+using namespace std;
 
 // Main Function
-int main(int argc, char* argv[])
+int main()
 {
+    int i;
+    const int NUM_ITEMS = 2;
+    string name;
+    int price;
+    int quantity;
+    double total;
+    ItemToPurchase item1;
+    ItemToPurchase item2;
+
+    for (i = 0; i < NUM_ITEMS; ++i) 
+    {
+        cout << "Item " << i + 1 << endl;
+        cout << "Enter the item name:" << endl;
+        cin.ignore(1000, '\n');
+        getline(cin, name);
+
+        cout << "Enter the item price:" << endl;
+        cin >> price;
+
+        cout << "Enter the item quantity:" << endl;
+        cin >> quantity;
+        cout << endl;
+
+        // Set the Object values for 1
+
+        // Set the Object values for 2
+
+
+
+        cin.ignore();
+    }
+
+    // Calculate the total
+
+    // Print Total
+    cout << "TOTAL COST" << endl;
+    item1.Print();
+    item2.Print();
+    cout << endl;
+    cout << "Total: $" << total << endl;
+
 
     return 0;
 }
-// Function Definitions
-
-
