98add9eb9152074358d4752b925f73e20882f786
stevensommer2 <46788601+stevensommer2@users.noreply.github.com>
2019-04-18 08:42:49 -0600

    hugos template

--- mod13/hw/PlaylistNode.h
+++ mod13/hw/PlaylistNode.h
@@ -17,7 +17,36 @@
  */
 #ifndef  PLAYLISTNODE__INC__
 #define  PLAYLISTNODE__INC__
-    <+DETAILED+>
+
+#include <string>
+using namespace std;
+
+class PlaylistNode 
+{
+    private:
+        string uniqueID;
+        string songName;
+        string artistName;
+        int songLength;
+        PlaylistNode* nextNodePtr;
+    public:
+        // Default Constructors
+        PlaylistNode();
+        // Constructors with parameters
+        PlaylistNode(string initID, string initSongName, string initArtistName,
+                int initSongLength, PlaylistNode* nextLoc = 0);
+        // Getters
+        string GetID() const;
+        string GetSongName() const;
+        string GetArtistName() const;
+        int GetSongLength() const;
+        // Other Methods
+        void InsertAfter(PlaylistNode* nodePtr);
+        void SetNext(PlaylistNode* nodePtr);
+        PlaylistNode* GetNext();
+        void PrintPlaylistNode();
+};
 
 #endif /* ----- #ifndef PLAYLISTNODE__INC__ ----- */
+
 
