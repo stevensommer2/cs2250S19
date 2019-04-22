1b3e7ce42481f35525ff18742568068462c7c24f
Steven Sommer <steven.sommer@mail.weber.edu>
2019-04-21 19:58:31 -0600

    initial code

---
+++ mod13/warmup/ContactNode.cpp
@@ -0,0 +1,40 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ContactNode.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  04/21/2019 07:57:32 PM
+ *       Revision:  none
+ *    Compiler(C):  gcc ContactNode.cpp -o ContactNode.out -lm
+ *  Compiler(C++):  g++ ContactNode.cpp -o ContactNode.out -lm
+            Usage:  ./ContactNode.out
+ *
+ *         Author:  Steven Sommer (), steven.sommer@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+//For C code
+#include <stdio.h>
+#include <stdlib.h>
+
+//For C++ code
+//#include <iostream>
+//using namespace std;
+
+// Constants and globals
+
+// Function Prototypes
+
+// Main Function
+int main(int argc, char* argv[])
+{
+
+    return 0;
+}
+// Function Definitions
+
+
---
+++ mod13/warmup/ContactNode.h
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ContactNode.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  04/21/2019 07:57:04 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Steven Sommer (), steven.sommer@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#ifndef  CONTACTNODE__INC__
+#define  CONTACTNODE__INC__
+    <+DETAILED+>
+
+#endif /* ----- #ifndef CONTACTNODE__INC__ ----- */
+
