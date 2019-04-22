de5b637388cb78066d14c9d842606aed5c9aad28
stevensommer2 <46788601+stevensommer2@users.noreply.github.com>
2019-04-21 20:08:31 -0600

    functioning code

--- mod13/warmup/main.cpp
+++ mod13/warmup/main.cpp
@@ -17,24 +17,70 @@
  *
  * =====================================================================================
  */
-//For C code
-#include <stdio.h>
-#include <stdlib.h>
 
-//For C++ code
-//#include <iostream>
-//using namespace std;
+#include <iostream>
+#include <iomanip>
+#include "ContactNode.h"
+using namespace std;
 
-// Constants and globals
-
-// Function Prototypes
-
-// Main Function
-int main(int argc, char* argv[])
+// Main Program
+int main() 
 {
+    string fullName;
+    string phoneNum;
+    ContactNode* headContact = 0;  // set pointer to "NULL" or 0
+    ContactNode* nextContact1 = 0;
+    ContactNode* nextContact2 = 0;
+    ContactNode* currContact = 0;
+
+    // Get first person
+    cout << "Person 1" << endl;
+    cout << "Enter name:" << endl;
+    getline(cin, fullName);
+    cout << "Enter phone number:" << endl;
+    cin >> phoneNum;
+    cout << "You entered: " << fullName << ", " << phoneNum << endl << endl;
+    headContact = new ContactNode(fullName, phoneNum, 0);
+    //First contact node (head of heap)
+    // use dynamically allocated memory here
+    // ... 
+    
+    // Get second person
+    cin.ignore();
+    cout << "Person 2" << endl;
+    cout << "Enter name:" << endl;
+    getline(cin, fullName);
+    cout << "Enter phone number:" << endl;
+    cin >> phoneNum;
+    cout << "You entered: " << fullName << ", " << phoneNum << endl << endl;
+    nextContact1 = new ContactNode(fullName, phoneNum, 0);
+    // Create a new contact here with "new" 
+    // Use the insertAfter to conect the two nodes
+    // Get third person
+    
+    cin.ignore();
+    cout << "Person 3" << endl;
+    cout << "Enter name:" << endl;
+    getline(cin, fullName);
+    cout << "Enter phone number:" << endl;
+    cin >> phoneNum;
+    cout << "You entered: " << fullName << ", " << phoneNum << endl << endl;
+    nextContact2 = new ContactNode(fullName, phoneNum, 0);
+
+    headContact->InsertAfter(nextContact1);
+    nextContact1->InsertAfter(nextContact2);
+
+    cout << "CONTACT LIST" << endl; 
+    // Loop over contacts
+    currContact=headContact;
+    while (currContact != 0) 
+    {
+        currContact->PrintContactNode();
+        currContact = currContact->GetNext();
+        cout << endl;
+    }
 
     return 0;
 }
-// Function Definitions
 
 
